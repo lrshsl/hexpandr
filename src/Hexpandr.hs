a0ffa06ff762e5209ebaf4ffbfc7c6a0afaf9c0c
lrshsl <hoeslilars@gmail.com>
2024-12-18 06:44:17 +0100

    Add tests (only prints) and alpha-numerical parsers

---
+++ src/Hexpandr.hs
@@ -0,0 +1,117 @@
+module Hexpandr where
+
+import Control.DeepSeq (force)
+
+------------------
+-- Parser types --
+------------------
+
+data Result ok err = Ok ok | Err err
+    deriving (Eq, Ord, Read, Show)
+
+data ParseError = ParseError { expected :: String, found :: String }
+    deriving (Eq, Show)
+
+type ParseResult = Result (String, String) ParseError
+
+type Parser = String -> ParseResult
+
+
+------------------
+-- Base parsers --
+------------------
+
+okParser :: Parser
+okParser = (\i -> Ok ([], i))
+
+
+anyChar :: Parser
+anyChar = \i -> case i of
+
+  (x:xs) -> (Ok ([x], xs))
+
+  []     -> Err $ ParseError { expected = "any character", found = "<EOI>" }
+
+
+eof :: Parser
+eof = \i -> case i of
+
+  []    -> Ok ([], [])
+
+  (c:_) -> Err $ ParseError { expected = "<EOI>", found = [c] }
+
+
+chr :: Char -> Parser
+chr c = \i -> case i of
+    (x:rst)
+        | x == c   -> Ok ([x], rst)
+        | otherwise -> Err $ ParseError { expected = [c], found = [x] }
+    [] -> Err $ ParseError { expected = [c], found = "<EOF>" }
+
+
+exactly :: String -> Parser
+exactly s = \i ->
+    case splitAt (length s) i of
+        (prefix, rst)
+            | prefix == s -> Ok (prefix, rst)
+            | otherwise   -> Err $ ParseError { expected = s, found = prefix }
+
+
+digit :: Parser
+digit = force oneOf (map chr "0123456789")
+
+
+alpha :: Parser
+alpha = force oneOf (map chr (['a' .. 'z'] ++ ['A' .. 'Z']))
+
+
+------------------------
+-- Parser combinators --
+------------------------
+
+oneOf :: [Parser] -> Parser
+oneOf parsers = foldl orElse (head parsers) (tail parsers)
+
+
+optional :: Parser -> Parser
+optional p = p `orElse` okParser
+
+
+andThen :: Parser -> Parser -> Parser
+a `andThen` b = \i ->
+    case a i of
+        Ok (s, rst) -> case b rst of
+            Ok (s_b, rst_b) -> Ok (s ++ s_b, rst_b)
+            err -> err
+        err -> err
+
+
+orElse :: Parser -> Parser -> Parser
+a `orElse` b = \i ->
+    case a i of
+        Err _ -> b i
+        ok -> ok
+
+
+many :: Parser -> Parser
+many p = \i ->
+    case p i of
+        Ok (s, rst) -> 
+            case many p rst of
+                Ok (s_b, rst_b) -> Ok (s ++ s_b, rst_b)
+                Err _ -> Ok (s, i)
+        Err _ -> Ok ([], i)
+
+
+sepByStrict :: Parser -> Parser -> Parser
+e `sepByStrict` s = e `andThen` ((s `andThen` (sepBy e s)) `orElse` okParser)
+
+sepBy :: Parser -> Parser -> Parser
+e `sepBy` s = e `andThen` ((s `andThen` (optional (sepBy e s))) `orElse` okParser)
+
+
+main :: IO ()
+main = do putStrLn "done"
+
+
+-- vim: et ts=4 sts=4 sw=4
---
+++ test/Main.hs
@@ -0,0 +1,49 @@
+
+module Main (Main.main) where
+
+import Hexpandr
+
+main :: IO ()
+main = do
+    let result1 = eof ""
+    print result1  -- Output: Ok []
+
+    let result2 = anyChar "Hello"
+    print result2
+
+    let result3 = eof "Hello"
+    print result3
+
+    let result4 = (anyChar `andThen` anyChar) "Hello"
+    print result4
+
+    let result5 = (many anyChar `andThen` eof) "Hello"
+    print result5
+
+    let result6 = chr '4' "Hello"
+    print result6
+
+    let result7 = exactly "Hel" "Hello"
+    print result7
+
+    let result8 = (digit `sepBy` chr ',') "1,2,3,4"
+    print result8
+
+    let result9 = (digit `sepBy` chr ',') "1,2,,3,4," -- Parses just fine with any number of commas, including trailing ones
+    let result10 = (digit `sepByStrict` chr ',') "1,2,,3,4," -- Does not parse additional commas and the last one
+    print result9
+    print result10
+
+    let result11 = (digit `sepBy` chr ',') ",1,2,3,4" -- Errors
+    print result11
+
+    let identch = oneOf [digit, alpha, chr '_']
+    let result12 = (identch `sepBy` chr ',') "a,2,Z,4"
+    print result12
+
+    let result13 = (identch `sepBy` chr ',') "a,2a,Z,4"
+    print result13
+
+    let result14 = (many identch `sepBy` chr ',') "a,2a,Z,nice_3"
+    print result14
+
