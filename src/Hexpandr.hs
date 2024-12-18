ef655677347b3f5a98d9814ca95cd7c87a77e788
lrshsl <hoeslilars@gmail.com>
2024-12-19 00:14:15 +0100

    Format code and rename

--- src/Hexpandr.hs
+++ src/Hexpandr.hs
@@ -2,21 +2,19 @@ module Hexpandr where
 
 import Control.DeepSeq (force)
 
-
 ------------------
 -- Parser types --
 ------------------
 
 data Result ok err = Ok ok | Err err
-    deriving (Eq, Ord, Read, Show)
+  deriving (Eq, Ord, Read, Show)
 
-
-type ParseResult = Result
+type ParseResult =
+  Result
     (String, String) -- Ok  (parsed, remaining)
     (String, String) -- Err (expected,   found)
 
 type Parser = String -> ParseResult
-
 
 ------------------
 -- Base parsers --
@@ -25,51 +23,41 @@ type Parser = String -> ParseResult
 okParser :: Parser
 okParser i = Ok ([], i)
 
-
 anyChar :: Parser
 anyChar i = case i of
-
-  (x:xs) -> Ok ([x], xs)
-
-  []     -> Err ("any character", "<EOI>")
-
+  (x : xs) -> Ok ([x], xs)
+  [] -> Err ("any character", "<EOI>")
 
 eof :: Parser
 eof i = case i of
+  [] -> Ok ([], [])
+  (c : _) -> Err ("<EOI>", [c])
 
-  []    -> Ok ([], [])
-
-  (c:_) -> Err ("<EOI>", [c])
-
-
-chr :: Char -> Parser
-chr c i = case i of
-    (x:rst)
-        | x == c    -> Ok ([x], rst)
-        | otherwise -> Err ([c], [x])
-    [] -> Err ([c], "<EOF>")
-
+charP :: Char -> Parser
+charP c i = case i of
+  (x : rst)
+    | x == c -> Ok ([x], rst)
+    | otherwise -> Err ([c], [x])
+  [] -> Err ([c], "<EOF>")
 
 exactly :: String -> Parser
 exactly s i =
-    case splitAt (length s) i of
-        (prefix, rst)
-            | prefix == s -> Ok (prefix, rst)
-            | otherwise   -> Err (s, prefix)
-
+  case splitAt (length s) i of
+    (prefix, rst)
+      | prefix == s -> Ok (prefix, rst)
+      | otherwise -> Err (s, prefix)
 
 digit :: Parser
-digit = force oneOf (map chr "0123456789")
+digit = force oneOf (map charP "0123456789")
 
 alpha :: Parser
-alpha = force oneOf (map chr (['a' .. 'z'] ++ ['A' .. 'Z']))
+alpha = force oneOf (map charP (['a' .. 'z'] ++ ['A' .. 'Z']))
 
 alphanum :: Parser
 alphanum = alpha `orElse` digit
 
 defaultIdent :: Parser
-defaultIdent = (alpha `orElse` chr '_') `andThen` many (alphanum `orElse` chr '_')
-
+defaultIdent = (alpha `orElse` charP '_') `andThen` many (alphanum `orElse` charP '_')
 
 ------------------------
 -- Parser combinators --
@@ -78,42 +66,36 @@ defaultIdent = (alpha `orElse` chr '_') `andThen` many (alphanum `orElse` chr '_
 oneOf :: [Parser] -> Parser
 oneOf = foldl1 orElse
 
-
 optional :: Parser -> Parser
 optional p = p `orElse` okParser
 
-
 andThen :: Parser -> Parser -> Parser
 a `andThen` b = \i ->
-    case a i of
-        Ok (s, rst) -> case b rst of
-            Ok (s_b, rst_b) -> Ok (s ++ s_b, rst_b)
-            err -> err
-        err -> err
-
+  case a i of
+    Ok (s, rst) -> case b rst of
+      Ok (s_b, rst_b) -> Ok (s ++ s_b, rst_b)
+      err -> err
+    err -> err
 
 orElse :: Parser -> Parser -> Parser
 a `orElse` b = \i ->
-    case a i of
-        Err (e, _) -> case b i of
-            Err (e_b, f) -> Err (e ++ e_b, f)
-            ok -> ok
-        ok -> ok
-
+  case a i of
+    Err (e, _) -> case b i of
+      Err (e_b, f) -> Err (e ++ e_b, f)
+      ok -> ok
+    ok -> ok
 
 many :: Parser -> Parser
 many p i =
-    case p i of
-        Ok (s, rst) ->
-            case many p rst of
-                Ok (s_b, rst_b) -> Ok (s ++ s_b, rst_b)
-                Err _ -> Ok (s, i)
-        Err _ -> Ok ([], i)
-
+  case p i of
+    Ok (s, rst) ->
+      case many p rst of
+        Ok (s_b, rst_b) -> Ok (s ++ s_b, rst_b)
+        Err _ -> Ok (s, i)
+    Err _ -> Ok ([], i)
 
 many1 :: Parser -> Parser
 many1 p = p `andThen` many p
-
 
 sepBy :: Parser -> Parser -> Parser
 e `sepBy` s = many ((e `andThen` s) `orElse` s) `andThen` optional e
@@ -121,16 +103,13 @@ e `sepBy` s = many ((e `andThen` s) `orElse` s) `andThen` optional e
 sepBy1 :: Parser -> Parser -> Parser
 e `sepBy1` s = e `andThen` (e `sepBy` s)
 
-
 sepByStrict :: Parser -> Parser -> Parser
 e `sepByStrict` s = optional (e `andThen` many (s `andThen` e))
 
 sepByStrict1 :: Parser -> Parser -> Parser
 e `sepByStrict1` s = e `andThen` (e `sepByStrict` s)
 
-
 main :: IO ()
 main = do putStrLn "done"
-
 
 -- vim: et ts=4 sts=4 sw=4
--- test/Main.hs
+++ test/Main.hs
@@ -1,101 +1,95 @@
-
 module Main (Main.main) where
 
 import Hexpandr
 
 main :: IO ()
 main = do
+  ---------------------------------------
+  --- Single characters
+  ---------------------------------------
 
-    ---------------------------------------
-    --- Single characters
-    ---------------------------------------
+  putStrLn "\n-- Single characters --"
 
-    putStrLn "\n-- Single characters --"
+  let parser1 = eof ""
+  print $ parser1 == Ok ("", "")
 
-    let parser1 = eof ""
-    print $ parser1 == Ok ("", "")
+  let parser2 = anyChar
+  print $ parser2 "Hello" == Ok ("H", "ello")
 
-    let parser2 = anyChar
-    print $ parser2 "Hello" == Ok ("H", "ello")
+  let parser3 = eof
+  print $ parser3 "Hello" == Err ("<EOI>", "H")
 
-    let parser3 = eof
-    print $ parser3 "Hello" == Err ("<EOI>", "H" )
+  let parser4 = anyChar `andThen` anyChar
+  print $ parser4 "Hello" == Ok ("He", "llo")
 
-    let parser4 = anyChar `andThen` anyChar
-    print $ parser4 "Hello" == Ok ("He", "llo")
+  let parser5 = many anyChar `andThen` eof
+  print $ parser5 "Hello" == Ok ("Hello", "")
 
-    let parser5 = many anyChar `andThen` eof
-    print $ parser5 "Hello" == Ok ("Hello", "")
+  let parser6 = charP '4'
+  print $ parser6 "Hello" == Err ("4", "H")
 
-    let parser6 = chr '4'
-    print $ parser6 "Hello" == Err ("4", "H")
+  let parser7 = exactly "Hel"
+  print $ parser7 "Hello" == Ok ("Hel", "lo")
 
-    let parser7 = exactly "Hel"
-    print $ parser7 "Hello" == Ok ("Hel", "lo")
+  ---------------------------------------
+  --- sepBy and sepByStrict
+  ---------------------------------------
 
+  putStrLn "\n-- SepBy parsers --"
 
-    ---------------------------------------
-    --- sepBy and sepByStrict
-    ---------------------------------------
+  let sepByParser = digit `sepBy` charP ','
+  let sepByStrictParser = digit `sepByStrict` charP ','
 
-    putStrLn "\n-- SepBy parsers --"
+  print $ sepByParser "1,2,3,4" == Ok ("1,2,3,4", "")
+  print $ sepByStrictParser "1,2,3,4" == Ok ("1,2,3,4", "")
 
-    let sepByParser = digit `sepBy` chr ','
-    let sepByStrictParser = digit `sepByStrict` chr ','
+  -- sepBy parses just fine with any number of commas, including trailing ones
+  print $ sepByParser "1,2,,3,4," == Ok ("1,2,,3,4,", "")
 
-    print $ sepByParser "1,2,3,4" == Ok ("1,2,3,4", "")
-    print $ sepByStrictParser "1,2,3,4" == Ok ("1,2,3,4", "")
+  -- Does not parse additional commas and the last one
+  print $ sepByStrictParser "1,2,,3,4," == Ok ("1,2", ",,3,4,")
 
-    -- sepBy parses just fine with any number of commas, including trailing ones
-    print $ sepByParser "1,2,,3,4," == Ok ("1,2,,3,4,", "")
+  ---------------------------------------
+  --- predefined alphanumerical+ sets
+  ---------------------------------------
 
-    -- Does not parse additional commas and the last one
-    print $ sepByStrictParser "1,2,,3,4," == Ok ("1,2", ",,3,4,")
+  putStrLn "\n-- Alphanumerical --"
 
+  let parser11 = digit `sepBy` charP ','
+  print $ parser11 ",1,2,3,4" == Ok (",1,2,3,4", "")
 
-    ---------------------------------------
-    --- predefined alphanumerical+ sets
-    ---------------------------------------
+  let identch = oneOf [digit, alpha, charP '_']
+  let parser12 = identch `sepBy` charP ','
+  print $ parser12 "a,2,Z,4" == Ok ("a,2,Z,4", "")
 
-    putStrLn "\n-- Alphanumerical --"
+  let parser13 = identch `sepBy` charP ','
+  print $ parser13 "a,2a,Z,4" == Ok ("a,2", "a,Z,4")
 
-    let parser11 = digit `sepBy` chr ','
-    print $ parser11 ",1,2,3,4" == Ok (",1,2,3,4", "")
+  let parser14 = many identch `sepBy` charP ','
+  print $ parser14 "a,2a,Z,nice_3" == Ok ("a,2a,Z,nice_3", "")
 
-    let identch = oneOf [digit, alpha, chr '_']
-    let parser12 = identch `sepBy` chr ','
-    print $ parser12 "a,2,Z,4" == Ok ("a,2,Z,4", "")
+  let parser15 = defaultIdent `sepBy` charP ','
+  print $ parser15 "a,Z,nice_3,a2" == Ok ("a,Z,nice_3,a2", "")
 
-    let parser13 = identch `sepBy` chr ','
-    print $ parser13 "a,2a,Z,4" == Ok ("a,2", "a,Z,4")
+  ---------------------------------------
+  --- Further examples
+  ---------------------------------------
 
-    let parser14 = many identch `sepBy` chr ','
-    print $ parser14 "a,2a,Z,nice_3" == Ok ("a,2a,Z,nice_3", "")
+  putStrLn "\n-- Further examples --"
 
-    let parser15 = defaultIdent `sepBy` chr ','
-    print $ parser15 "a,Z,nice_3,a2" == Ok ("a,Z,nice_3,a2", "")
+  let identP = alpha `andThen` many (oneOf [alpha, digit, charP '_'])
+  print $ identP "some_ident42" == Ok ("some_ident42", "")
 
+  let floatP = many1 digit `andThen` optional (charP '.' `andThen` many digit)
+  print $ floatP "32.01" == Ok ("32.01", "")
+  print $ floatP "1" == Ok ("1", "")
+  print $ floatP "a" == Err (['0' .. '9'], "a")
 
-    ---------------------------------------
-    --- Further examples
-    ---------------------------------------
+  let intP = many1 digit
 
-    putStrLn "\n-- Further examples --"
+  let arrayElement = oneOf [identP, floatP, intP]
+  let arrayP = charP '[' `andThen` (arrayElement `sepBy` charP ' ') `andThen` charP ']'
 
-    let identP = alpha `andThen` many (oneOf [alpha, digit, chr '_'])
-    print $ identP "some_ident42" == Ok ("some_ident42", "")
+  print $ arrayP "[a 3.5 b 90]" == Ok ("[a 3.5 b 90]", "")
 
-    let floatP = many1 digit `andThen` optional (chr '.' `andThen` many digit)
-    print $ floatP "32.01" == Ok ("32.01", "")
-    print $ floatP "1" == Ok ("1", "")
-    print $ floatP "a" == Err (['0' .. '9'], "a")
-
-    let intP = many1 digit
-
-    let arrayElement = oneOf [identP, floatP, intP]
-    let arrayP = chr '[' `andThen` (arrayElement `sepBy` chr ' ') `andThen` chr ']'
-
-    print $ arrayP "[a 3.5 b 90]" == Ok ("[a 3.5 b 90]", "")
-
-    putStrLn "\nTests Done\n"
-
+  putStrLn "\nTests Done\n"
